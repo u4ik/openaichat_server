2c0d07cadce0836d3cd3c944801353bcaed80cdb
u4ik <insighteuphoric@gmail.com>
2023-02-05 13:10:02 -0500

    Image compression/Preventions

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
-const { Configuration, OpenAIApi } = require("openai");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const express = require("express");
 const fs = require('fs');
 const multer = require('multer');
+const { Configuration, OpenAIApi } = require("openai");
+const sharp = require('sharp');
+
 const token = process.env.API_TOKEN
 const configuration = new Configuration({ apiKey: token });
 const openai = new OpenAIApi(configuration);
-
-const sharp = require('sharp');
 
 const app = express();
 app.use(cors());
@@ -21,53 +21,67 @@ const IMAGE_SIZE = "256x256";
 
 const storage = multer.diskStorage({
     destination: __dirname + `/images`,
+    limits: {
+        fileSize: 10000000 // 10000000 Bytes = 10 MB
+    },
     filename: (req, file, cb) => {
-        cb(null, 'ai.png')
+        try {
+            if (!/^image/.test(file.mimetype)) throw new Error("Please upload an Image.");
+            cb(null, 'ai.png')
+        } catch (error) {
+            console.log(error)
+        };
     }
 });
 
 const upload = multer({
     storage: storage,
     fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(png|jpg|jpeg)$/)) {
-            cb(new Error('Please upload an image.'))
-        }
-        cb(undefined, true)
+        if (!file.originalname.match(/\.(png|jpg|jpeg)$/)) cb(new Error('Please upload an image.'));
+        cb(undefined, true);
     }
 });
 
 app.post("/alter_image", upload.single('upload'), async (req, res) => {
     try {
+        if (!/^image/.test(req.file.mimetype)) throw new Error("Please upload an Image.");
+        if (req.file.size >= 40_000_000) throw new Error("File size is too big, please try it again with a file that is around 4MB");
+
         await sharp(__dirname + '/images/ai.png')
             .resize(500, 500)
             .toFormat('png')
-            .toFile(__dirname + '/images/output.png')
+            .toFile(__dirname + '/images/output.png');
 
-        const compressedFileSize = fs.statSync(__dirname + "/images/output.png").size
-        if (compressedFileSize >= 3_900_000) throw new Error("File size is too big, please try it again with a file that is less than 4MB")
+
+        const compressedFileSize = fs.statSync(__dirname + "/images/output.png").size;
+        if (compressedFileSize >= 3_900_000) throw new Error("File size is too big, please try it again with a file that is less than 4MB");
 
         const response = await openai.createImageVariation(
-            // fs.createReadStream(__dirname + "/images/ai.png"),
             fs.createReadStream(__dirname + "/images/output.png"),
             MAX_NUMBER,
             IMAGE_SIZE
-        )
+        );
+
         fs.unlinkSync(__dirname + "/images/ai.png");
         fs.unlinkSync(__dirname + "/images/output.png");
-        res.status(200).json({ res: response.data.data });
 
+        res.status(200).json({ res: response.data.data });
     } catch (error) {
-        if (error.response) {
-            console.log(error.response.data);
-            res.status(500).json({ message: error.response.data })
-            console.log(error.response.status);
-            res.status(500).json({ message: error.response.status })
-        } else {
-            console.log(error.message);
-            res.status(500).json({ message: error.message })
-        }
-    }
-})
+        switch (error) {
+            case error.response:
+                console.log(error.response.data);
+                res.status(500).json({ message: error.response.data });
+                break;
+            case error.message:
+                console.log(error.message);
+                res.status(500).json({ message: error.message });
+                break;
+            default:
+                console.log(error)
+                res.status(500).json({ message: error });
+        };
+    };
+});
 
 app.post("/message", (req, res) => {
     const response = openai.createCompletion({
@@ -78,11 +92,11 @@ app.post("/message", (req, res) => {
         frequency_penalty: 0,
         presence_penalty: 0,
         max_tokens: 1024
-    })
+    });
     response.then(data => {
         res.send({ message: data.data.choices[0].text })
-    })
-})
+    });
+});
 
 app.post("/image", async (req, res) => {
     try {
@@ -95,9 +109,9 @@ app.post("/image", async (req, res) => {
     } catch (err) {
         console.log(err);
         res.status(500).json({ message: response })
-    }
-})
+    };
+});
 
 app.listen(process.env.PORT, () => {
-    console.log(`App is spinning on port ${process.env.PORT}`)
-})
+    console.log(`App is spinning on port ${process.env.PORT}`);
+});
